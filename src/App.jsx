8ad9b7ed2cac37a79cc451b209dcbb4232399f78
mutlib-ansari <mutlib.indianit@gmail.com>
2025-01-15 12:18:09 +0530

    mutlib

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/header/header";
+import Header from "./components/Header/Header";
 
 
 
